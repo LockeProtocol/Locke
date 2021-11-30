d419679fdbf4024f95bcec0d3a79761aa8e22dca
Brock <brock.elmore@gmail.com>
2021-11-29 22:08:38 -0800

    no merkle

--- src/Locke.sol
+++ src/Locke.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import "lib/openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
 import "./LockeERC20.sol";
 import "solmate/utils/SafeTransferLib.sol";
 import "solmate/tokens/ERC20.sol";
