bd378ef51886ae2407b510e840504c51f2b3ec5a
0xdef1 <0xdef1.eth@gmail.com>
2022-02-04 15:07:21 -0500

    Deploy StreamCreation and MerkleStreamCreation before factory

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,12 @@ set -eo pipefail
 . $(dirname $0)/common.sh
 
 # Deploy.
-StreamFactoryAddr=$(deploy StreamFactory Locke.sol $ETH_FROM $ETH_FROM)
+
+StreamCreation=$(deploy StreamCreation LockeFactory.sol)
+log "StreamCreation deployed at: " $StreamCreation
+
+MerkleStreamCreation=$(deploy MerkleStreamCreation LockeFactory.sol)
+log "MerkleStreamCreation deployed at: " $MerkleStreamCreation
+
+StreamFactoryAddr=$(deploy StreamFactory LockeFactory.sol $ETH_FROM $ETH_FROM $StreamCreation $MerkleStreamCreation)
 log "StreamFactory deployed at:" $StreamFactoryAddr
